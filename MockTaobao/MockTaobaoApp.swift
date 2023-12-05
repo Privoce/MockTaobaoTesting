8887f42d2052974bcaaf92e62a7a943361aa0e90
shuotwang <shuotongwang@gmail.com>
2023-12-05 15:54:21 -0800

    Initial Commit

---
+++ MockTaobao/MockTaobaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockTaobaoApp.swift
+//  MockTaobao
+//
+//  Created by Vincent Wang on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MockTaobaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
